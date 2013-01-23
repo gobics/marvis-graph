469eda4acc8c874d0574f355da2aa4010fd0d429
Manuel Landesfeind <manuel@gobics.de>
2013-01-23 12:24:32 +0100

    Using graph events in cofactor selection (Bug: otherwise graph-layout is calculated two times)

--- src/de/gobics/marvis/graph/gui/actions/ViewerActionDrawDefineCofactor.java
+++ src/de/gobics/marvis/graph/gui/actions/ViewerActionDrawDefineCofactor.java
@@ -8,8 +8,11 @@ import de.gobics.marvis.graph.graphview.GraphViewCustomizable;
 import de.gobics.marvis.graph.gui.DialogSpinner;
 import de.gobics.marvis.graph.gui.graphvisualizer.VisualizationViewerGraph;
 import java.awt.Dialog.ModalityType;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 /**
@@ -32,24 +35,23 @@ public class ViewerActionDrawDefineCofactor extends AbstractViewerAction {
 	public void actionPerformed(ActionEvent e) {
 		int current_value = view.getCofactorLimit();
 		if (current_value < 0) {
-			current_value = 0;
+			current_value = 25;
 		}
-		DialogSpinner dialog = new DialogSpinner(null, "Select cofactor limit", ModalityType.APPLICATION_MODAL,
-				new SpinnerNumberModel(current_value, 0, Integer.MAX_VALUE, 1));
-		dialog.setVisible(true);
 
-		if (dialog.aborted()) {
+		SpinnerNumberModel model = new SpinnerNumberModel(current_value, 0, Integer.MAX_VALUE, 1);
+		JSpinner spinner = new JSpinner(model);
+		spinner.setMaximumSize(new Dimension(300, 100));
+		int result = JOptionPane.showConfirmDialog(null, spinner, "Select cofactor limit", JOptionPane.OK_CANCEL_OPTION);
+		if (result != JOptionPane.OK_OPTION) {
 			return;
 		}
 
-		Number n = (Number) dialog.getValue();
+		Number n = (Number) spinner.getValue();
 		if (n.intValue() == view.getCofactorLimit()) {
 			return;
 		}
 
-		Logger.getLogger(ViewerActionDrawDefineCofactor.class.getName()).finer("Setting cofactor limit to: " + n.
-				intValue());
+		Logger.getLogger(ViewerActionDrawDefineCofactor.class.getName()).finer("Setting cofactor limit to: " + n.intValue());
 		view.setCofactorLimit(n.intValue());
-		viewer.updateGraphLayout();
 	}
 }
