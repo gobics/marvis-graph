b47481007e21ca4e7c86e6ad1a6627337ef6c5c0
Manuel Landesfeind <manuel@gobics.de>
2013-01-29 11:29:00 +0100

    Load several networks for permutation testing

--- src/de/gobics/marvis/graph/test.java
+++ src/de/gobics/marvis/graph/test.java
@@ -20,31 +20,32 @@ public class test {
 
 	private final static Logger logger = Logger.getLogger(test.class.getName());
 	private static double restart_probability = 0.8;
-	private static final int NUM_PERMUTES = 10;
+	private static final int NUM_PERMUTES = 1000;
 	private static final int COFACTOR_THRESHOLD = 10;
 
 	public static void main(String[] args) throws Exception {
 		LoggingUtils.initLogger(Level.FINER);
 		// Load the network
 		logger.finer("Loading network");
-		LoadNetwork loader = new LoadNetwork("testdata/wound1/wound1.complete.cut5000.intNames.xml.gz");
-		final MetabolicNetwork network = loader.load();
+		for (String file : args) {
+			LoadNetwork loader = new LoadNetwork(file);
+			final MetabolicNetwork network = loader.load();
 
+			CalculateNetworksRWR process = new CalculateNetworksRWR(network);
+			process.setCofactorThreshold(COFACTOR_THRESHOLD);
+			process.setRestartProbability(restart_probability);
+			MetabolicNetwork[] subs = process.calculateNetworks(network);
 
-		CalculateNetworksRWR process = new CalculateNetworksRWR(network);
-		process.setCofactorThreshold(COFACTOR_THRESHOLD);
-		process.setRestartProbability(restart_probability);
-		MetabolicNetwork[] subs = process.calculateNetworks(network);
+			PermutationTest test = new PermutationTest(network, subs, process, new NetworkSorterSumOfWeights(network));
+			test.setNumberOfPermutations(NUM_PERMUTES);
+			Set<PermutationTestResult> networks = test.calculateScores();
 
-		PermutationTest test = new PermutationTest(network, subs, process, new NetworkSorterSumOfWeights(network));
-		test.setNumberOfPermutations(NUM_PERMUTES);
-		Set<PermutationTestResult> networks = test.calculateScores();
-
-		BufferedWriter out = new BufferedWriter(new FileWriter("scores.csv"));
-		out.write("Name\tnum_reactions\tscore\tfwer\nfdr\n");
-		for (PermutationTestResult r : networks) {
-			out.write(r.network.getName() + "\t" +r.network.getReactions().size()+"\t"+ r.score + "\t" + r.fwer + "\t" + r.fdr + "\n");
+			BufferedWriter out = new BufferedWriter(new FileWriter(file + ".scores.csv"));
+			out.write("Name\tnum_reactions\tscore\tfwer\nfdr\n");
+			for (PermutationTestResult r : networks) {
+				out.write(r.network.getName() + "\t" + r.network.getReactions().size() + "\t" + r.score + "\t" + r.fwer + "\t" + r.fdr + "\n");
+			}
+			out.close();
 		}
-		out.close();
 	}
 }
