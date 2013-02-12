03b938dfa6f53a1df3c207528984fcce7b74bed4
Manuel Landesfeind <manuel@gobics.de>
2013-02-12 15:48:33 +0100

    Calculate stepwise permutation scores

--- src/de/gobics/marvis/graph/test.java
+++ src/de/gobics/marvis/graph/test.java
@@ -2,8 +2,8 @@ package de.gobics.marvis.graph;
 
 import de.gobics.marvis.graph.gui.tasks.CalculateNetworksRWR;
 import de.gobics.marvis.graph.gui.tasks.LoadNetwork;
-import de.gobics.marvis.graph.gui.tasks.PermutationTestResult;
 import de.gobics.marvis.graph.gui.tasks.PermutationTest;
+import de.gobics.marvis.graph.gui.tasks.PermutationTestResult;
 import de.gobics.marvis.graph.sort.NetworkSorterSumOfWeights;
 import de.gobics.marvis.utils.LoggingUtils;
 import java.io.BufferedWriter;
@@ -20,7 +20,7 @@ public class test {
 
 	private final static Logger logger = Logger.getLogger(test.class.getName());
 	private static double restart_probability = 0.8;
-	private static final int NUM_PERMUTES = 100;
+	private static final int NUM_PERMUTES = 1000;
 	private static final int COFACTOR_THRESHOLD = 10;
 
 	public static void main(String[] args) throws Exception {
@@ -31,22 +31,29 @@ public class test {
 			LoadNetwork loader = new LoadNetwork(file);
 			final MetabolicNetwork network = loader.load();
 
-			CalculateNetworksRWR process = new CalculateNetworksRWR(network);
-			process.setCofactorThreshold(COFACTOR_THRESHOLD);
-			process.setRestartProbability(restart_probability);
-			MetabolicNetwork[] subs = process.calculateNetworks(network);
-			
-			PermutationTest test = new PermutationTest(network, subs, process, new NetworkSorterSumOfWeights(network));
-			test.setNumberOfPermutations(NUM_PERMUTES);
-			
-			Set<PermutationTestResult> networks = test.calculateScores();
+			for (double rp = 0.9d; rp >= 0; rp -= 0.1) {
+				logger.info("Permutating for restart probability of: "+rp);
+				CalculateNetworksRWR process = new CalculateNetworksRWR(network);
+				process.setCofactorThreshold(COFACTOR_THRESHOLD);
+				process.setRestartProbability(rp);
+				MetabolicNetwork[] subs = process.calculateNetworks(network);
 
-			BufferedWriter out = new BufferedWriter(new FileWriter(file + ".scores.csv"));
-			out.write("Name\tnum_reactions\tscore\tfwer\tfdr\n");
-			for (PermutationTestResult r : networks) {
-				out.write(r.network.getName() + "\t" + r.network.getReactions().size() + "\t" + r.score + "\t" + r.fwer + "\t" + r.fdr + "\n");
+				PermutationTest test = new PermutationTest(network, subs, process, new NetworkSorterSumOfWeights(network));
+				test.setNumberOfPermutations(NUM_PERMUTES);
+
+				Set<PermutationTestResult> networks = test.calculateScores(true);
+
+				String resultfilename = file + ".scores-"+rp+".csv";
+				logger.info("Writing result to: "+resultfilename);
+				BufferedWriter out = new BufferedWriter(new FileWriter(resultfilename));
+				out.write("Name\tnum_reactions\tscore\tfwer\tfdr\n");
+				for (PermutationTestResult r : networks) {
+					out.write(r.network.getName() + "\t" + r.network.getReactions().size() + "\t" + r.score + "\t" + r.fwer + "\t" + r.fdr + "\n");
+				}
+				out.close();
+				
+				logger.info("Calculations for restart probability "+rp+" is ready");
 			}
-			out.close();
 		}
 	}
 }
