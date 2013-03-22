0fc6d1aee1d1e524448bfe49640290fc6420af9a
Manuel Landesfeind <manuel@gobics.de>
2013-03-22 10:52:52 +0100

    Use static widgets to store parameters

--- src/de/gobics/marvis/graph/gui/DialogCalculateNetworks.java
+++ src/de/gobics/marvis/graph/gui/DialogCalculateNetworks.java
@@ -26,97 +26,98 @@ import javax.swing.SwingWorker;
  */
 public class DialogCalculateNetworks extends DialogAbstract {
 
-    private final ComboboxAlgorithm cb_algorithm = new ComboboxAlgorithm();
-    private final SpinnerNumberModel sm_gaps = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
-    private final SpinnerNumberModel sm_cofactor = new SpinnerNumberModel(10, 1, Integer.MAX_VALUE, 1);
-    private final SpinnerNumberModel sm_rwr = new SpinnerNumberModel(0.8, 0, 1, 0.1);
-	private final JCheckBox use_input_weights = new JCheckBox("", false);
-    private final JPanel options_panel = new JPanel(new BorderLayout());
-    private final JPanel options_rwr = new OptionsRWR();
-    private final JPanel options_reaction = new OptionsReaction();
+	private static final ComboboxAlgorithm cb_algorithm = new ComboboxAlgorithm();
+	private static final SpinnerNumberModel sm_gaps = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
+	private static final SpinnerNumberModel sm_cofactor = new SpinnerNumberModel(10, 1, Integer.MAX_VALUE, 1);
+	private static final SpinnerNumberModel sm_rwr = new SpinnerNumberModel(0.8, 0, 1, 0.1);
+	private static final SpinnerNumberModel sm_rwr_threshold = new SpinnerNumberModel(0.2, 0, 1, 0.1);
+	private static final JCheckBox use_input_weights = new JCheckBox("", false);
+	private final JPanel options_panel = new JPanel(new BorderLayout());
+	private final JPanel options_rwr = new OptionsRWR();
+	private final JPanel options_reaction = new OptionsReaction();
 
-    public DialogCalculateNetworks(Frame window) {
-	super(window, "Calculate sub networks", ModalityType.DOCUMENT_MODAL);
+	public DialogCalculateNetworks(Frame window) {
+		super(window, "Calculate sub networks", ModalityType.DOCUMENT_MODAL);
 
-	final JPanel algorithm_panel = new JPanel(new SpringLayout());
-	algorithm_panel.add(new JLabel("Select algorithm:"));
-	algorithm_panel.add(cb_algorithm);
-	SpringUtilities.makeCompactGrid(algorithm_panel);
+		final JPanel algorithm_panel = new JPanel(new SpringLayout());
+		algorithm_panel.add(new JLabel("Select algorithm:"));
+		algorithm_panel.add(cb_algorithm);
+		SpringUtilities.makeCompactGrid(algorithm_panel);
 
-	options_panel.add(algorithm_panel, BorderLayout.PAGE_START);
-	options_panel.add(options_rwr, BorderLayout.CENTER);
-
-	addOptions(options_panel);
-
-	cb_algorithm.addActionListener(new ActionListener() {
-	    @Override
-	    public void actionPerformed(ActionEvent ae) {
-		options_panel.removeAll();
 		options_panel.add(algorithm_panel, BorderLayout.PAGE_START);
+		options_panel.add(options_rwr, BorderLayout.CENTER);
 
+		addOptions(options_panel);
+
+		cb_algorithm.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent ae) {
+				options_panel.removeAll();
+				options_panel.add(algorithm_panel, BorderLayout.PAGE_START);
+
+				if (cb_algorithm.getSelectedIndex() == 0) {
+					options_panel.add(options_rwr, BorderLayout.CENTER);
+				}
+				else {
+					if (cb_algorithm.getSelectedIndex() == 1) {
+						options_panel.add(options_reaction, BorderLayout.CENTER);
+					}
+				}
+				pack();
+			}
+		});
+	}
+
+	public AbstractNetworkCalculation getTask(MetabolicNetwork n) {
 		if (cb_algorithm.getSelectedIndex() == 0) {
-		    options_panel.add(options_rwr, BorderLayout.CENTER);
-		}
-		else {
-		    if (cb_algorithm.getSelectedIndex() == 1) {
-			options_panel.add(options_reaction, BorderLayout.CENTER);
-		    }
-		}
-		pack();
-	    }
-	});
-    }
-
-    public AbstractNetworkCalculation getTask(MetabolicNetwork n) {
-	if (cb_algorithm.getSelectedIndex() == 0) {
-	    CalculateNetworksRWR process = new CalculateNetworksRWR(n);
-	    process.setRestartProbability(sm_rwr.getNumber().doubleValue());
+			CalculateNetworksRWR process = new CalculateNetworksRWR(n);
+			process.setRestartProbability(sm_rwr.getNumber().doubleValue());
 	    process.setCofactorThreshold(sm_cofactor.getNumber().intValue());
-		process.useInputWeights(use_input_weights.isSelected());
-	    return process;
+			process.useInputWeights(use_input_weights.isSelected());
+			return process;
+		}
+		if (cb_algorithm.getSelectedIndex() == 1) {
+			CalculateNetworksReaction process = new CalculateNetworksReaction(n);
+			process.setMaximumGaps(sm_gaps.getNumber().intValue());
+			process.setCofactorTreshold(sm_cofactor.getNumber().intValue());
+			return process;
+		}
+
+		return new CalculateNetworksPathway(n);
 	}
-	if (cb_algorithm.getSelectedIndex() == 1) {
-	    CalculateNetworksReaction process = new CalculateNetworksReaction(n);
-	    process.setMaximumGaps(sm_gaps.getNumber().intValue());
-	    process.setCofactorTreshold(sm_cofactor.getNumber().intValue());
-	    return process;
+
+	private static class ComboboxAlgorithm extends JComboBox {
+
+		public ComboboxAlgorithm() {
+			addItem("Random walk based");
+			addItem("Reactions");
+			addItem("Pathways");
+		}
 	}
 
-	return new CalculateNetworksPathway(n);
-    }
+	private static class OptionsReaction extends JPanel {
 
-    private class ComboboxAlgorithm extends JComboBox {
-
-	public ComboboxAlgorithm() {
-	    addItem("Random walk based");
-	    addItem("Reactions");
-	    addItem("Pathways");
+		public OptionsReaction() {
+			super(new SpringLayout());
+			add(new JLabel("Maximum gaps:"));
+			add(new JSpinner(sm_gaps));
+			add(new JLabel("Cofactor treshold:"));
+			add(new JSpinner(sm_cofactor));
+			SpringUtilities.makeCompactGrid(this);
+		}
 	}
-    }
 
-    private class OptionsReaction extends JPanel {
+	private static class OptionsRWR extends JPanel {
 
-	public OptionsReaction() {
-	    super(new SpringLayout());
-	    add(new JLabel("Maximum gaps:"));
-	    add(new JSpinner(sm_gaps));
+		public OptionsRWR() {
+			super(new SpringLayout());
+			add(new JLabel("Restart probability:"));
+			add(new JSpinner(sm_rwr));
 	    add(new JLabel("Cofactor treshold:"));
 	    add(new JSpinner(sm_cofactor));
-	    SpringUtilities.makeCompactGrid(this);
+			add(new JLabel("Use input weights for initial scoring:"));
+			add(use_input_weights);
+			SpringUtilities.makeCompactGrid(this);
+		}
 	}
-    }
-
-    private class OptionsRWR extends JPanel {
-
-	public OptionsRWR() {
-	    super(new SpringLayout());
-	    add(new JLabel("Restart probability:"));
-	    add(new JSpinner(sm_rwr));
-	    add(new JLabel("Cofactor treshold:"));
-	    add(new JSpinner(sm_cofactor));
-		add(new JLabel("Use input weights for initial scoring:"));
-		add(use_input_weights);
-	    SpringUtilities.makeCompactGrid(this);
-	}
-    }
 }
