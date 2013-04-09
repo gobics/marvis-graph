89ed8dad91eb63b6b92aa9ef92b6b6678cd99965
Manuel Landesfeind <manuel@gobics.de>
2013-04-09 14:51:10 +0200

    Add more output to network report

--- src/de/gobics/marvis/graph/downloader/MetabolicNetworkTester.java
+++ src/de/gobics/marvis/graph/downloader/MetabolicNetworkTester.java
@@ -12,24 +12,30 @@ import java.util.logging.Logger;
  * @author Manuel Landesfeind &lt;manuel@gobics.de&gt;
  */
 public class MetabolicNetworkTester {
-
+	
 	private static final Logger logger = Logger.getLogger(MetabolicNetworkTester.class.
 			getName());
 	private final MetabolicNetwork network;
-
+	
 	public MetabolicNetworkTester(MetabolicNetwork network) {
 		this.network = network;
 	}
-
+	
 	public String generateReport() {
 		StringBuilder sb = new StringBuilder("Report for metabolic network: " + network.
 				getName() + "\n");
-
+		
 		sb.append("Network contains:\n");
 		sb.append("  Compounds: ").append(network.getCompounds().size()).append("\n");
+		if (network.hasMarkers()) {
+			sb.append("            ").append(countCompoundsWithMarker()).append(" with marker candidate\n");
+		}
 		sb.append("  Reactions: ").append(network.getReactions().size()).append("\n");
 		sb.append("  Enzymes:   ").append(network.getEnzymes().size()).append("\n");
 		sb.append("  Genes:     ").append(network.getGenes().size()).append("\n");
+		if (network.hasTranscripts()) {
+			sb.append("            ").append(countGenesWithTranscript()).append(" with transcript\n");
+		}
 		sb.append("  Pathways:  ").append(network.getPathways().size()).append("\n");
 		if (network.hasMarkers()) {
 			sb.append("  Marker  :  ").append(network.getMarkers().size()).
@@ -39,7 +45,7 @@ public class MetabolicNetworkTester {
 			sb.append("  Transcripts:  ").append(network.getTranscripts().size()).
 					append("\n");
 		}
-
+		
 		sb.append("\nThere are...\n");
 		sb.append("  ").append(countCompoundWithoutReaction()).append(" compound without reaction\n");
 		sb.append("  ").append(countReactionWithoutPathway()).append(" reactions without pathway\n");
@@ -49,18 +55,22 @@ public class MetabolicNetworkTester {
 			sb.append("  ").append(countMarkerWithoutReaction()).append(" marker without link to reaction (").
 					append(network.getMarkers().size() - countMarkerWithoutReaction()).
 					append(" with)\n");
-
+			int count = countUniqueReactionsReachableFrom(network.getMarkers());
+			sb.append("     reaching ").append(count).append(" distinct reactions\n");
+			
 		}
 		if (network.hasTranscripts()) {
 			sb.append("  ").append(countTranscriptsWithoutReaction()).append(" transcripts without link to reaction(").
 					append(network.getTranscripts().size() - countTranscriptsWithoutReaction()).
 					append(" with)\n");
+			int count = countUniqueReactionsReachableFrom(network.getTranscripts());
+			sb.append("     reaching ").append(count).append(" distinct reactions\n");
 		}
-
-
+		
+		
 		return sb.toString();
 	}
-
+	
 	public int countCompoundWithoutReaction() {
 		int counter = 0;
 		for (Compound c : network.getCompounds()) {
@@ -70,7 +80,7 @@ public class MetabolicNetworkTester {
 		}
 		return counter;
 	}
-
+	
 	public int countReactionWithoutPathway() {
 		int counter = 0;
 		for (Reaction r : network.getReactions()) {
@@ -80,7 +90,7 @@ public class MetabolicNetworkTester {
 		}
 		return counter;
 	}
-
+	
 	public int countEnzymesWithoutReaction() {
 		int counter = 0;
 		for (Enzyme e : network.getEnzymes()) {
@@ -90,7 +100,7 @@ public class MetabolicNetworkTester {
 		}
 		return counter;
 	}
-
+	
 	public int countGenesWithoutEnzymes() {
 		int counter = 0;
 		for (Gene e : network.getGenes()) {
@@ -100,41 +110,80 @@ public class MetabolicNetworkTester {
 		}
 		return counter;
 	}
-
+	
 	public int countMarkerWithoutReaction() {
 		int counter = 0;
 		for (Marker m : network.getMarkers()) {
 			Set<Reaction> reactions = new TreeSet<Reaction>();
-
+			
 			for (Compound c : network.getAnnotations(m)) {
 				reactions.addAll(network.getReactions(c));
 			}
-
+			
 			if (!assertFound(reactions)) {
 				counter++;
 			}
 		}
 		return counter;
 	}
-
+	
 	public int countTranscriptsWithoutReaction() {
 		int counter = 0;
 		for (Transcript t : network.getTranscripts()) {
 			Set<Reaction> reactions = new TreeSet<Reaction>();
-
+			
 			for (Gene g : network.getGenes(t)) {
 				for (Enzyme e : network.getEncodedEnzymes(g)) {
 					reactions.addAll(network.getReactions(e));
 				}
 			}
-
+			
 			if (!assertFound(reactions)) {
 				counter++;
 			}
 		}
 		return counter;
 	}
-
+	
+	public int countGenesWithTranscript() {
+		int counter = 0;
+		for (Gene e : network.getGenes()) {
+			if (assertFound(network.getTranscripts(e))) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	
+	public int countCompoundsWithMarker() {
+		int counter = 0;
+		for (Compound c : network.getCompounds()) {
+			if (assertFound(network.getAnnotatingMarker(c))) {
+				counter++;
+			}
+		}
+		return counter;
+	}
+	
+	public int countUniqueReactionsReachableFrom(Collection<? extends GraphObject> objs) {
+		Set<Reaction> rs = new TreeSet<Reaction>();
+		for (GraphObject o : objs) {
+			if (o instanceof Transcript) {
+				for (Gene g : network.getGenes((Transcript) o)) {
+					for (Enzyme e : network.getEncodedEnzymes(g)) {
+						rs.addAll(network.getReactions(e));
+					}
+				}
+			}
+			else if (o instanceof Marker) {
+				for (Compound c : network.getAnnotations((Marker) o)) {
+					rs.addAll(network.getReactions(c));
+				}
+			}
+		}
+		return rs.size();
+	}
+	
 	private boolean assertFound(Collection<? extends GraphObject> list) {
 		return list != null && !list.isEmpty();
 	}
