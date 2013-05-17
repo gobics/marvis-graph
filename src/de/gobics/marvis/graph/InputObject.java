540062357462c91338f06a7c406f92b232ea8bcf
Manuel Landesfeind <manuel@gobics.de>
2013-05-17 14:27:18 +0200

    Handle direct annotations in input object

--- src/de/gobics/marvis/graph/InputObject.java
+++ src/de/gobics/marvis/graph/InputObject.java
@@ -1,26 +1,29 @@
 package de.gobics.marvis.graph;
 
 /**
- * An InputObject is an abstract subclass of the {@link GraphObject}. An input object
- * is some entity that has been measured from experimental context. All input object
- * contain an {@link IntensityProfile} and a weight.
+ * An InputObject is an abstract subclass of the {@link GraphObject}. An input
+ * object is some entity that has been measured from experimental context. All
+ * input object contain an {@link IntensityProfile} and a weight.
+ *
  * @author Manuel Landesfeind <manuel@gobics.de>
  */
-
 public abstract class InputObject extends GraphObject implements HasIntensity {
+
 	/**
-	 * The weight of this input object. The weight can be any type of double value
-	 * and indicates some preprocessed value. It can be e.g. a p-Value.
+	 * The weight of this input object. The weight can be any type of double
+	 * value and indicates some preprocessed value. It can be e.g. a p-Value.
 	 */
 	private double weight = 0.0;
 	/**
 	 * The intensity profile for this input object.
 	 */
 	private IntensityProfile intensities = new IntensityProfile();
+	private String annotation_id = null;
 
-	
 	/**
-	 * A string containing the ID of this object must be given to the super class.
+	 * A string containing the ID of this object must be given to the super
+	 * class.
+	 *
 	 * @see GraphObject
 	 * @param id the id of this object.
 	 */
@@ -29,15 +32,16 @@ public abstract class InputObject extends GraphObject implements HasIntensity {
 	}
 
 	/**
-	 * Set the weight of this object. Weight has to be a double value from the interval
-	 * 0 to 1 (both including).
+	 * Set the weight of this object. Weight has to be a double value from the
+	 * interval 0 to 1 (both including).
+	 *
 	 * @param weight the new weight
 	 */
 	public void setWeight(Double weight) {
 		if (weight == null || weight < 0) {
 			weight = 0d;
 		}
-		if ( weight > 1 ) {
+		if (weight > 1) {
 			weight = 1d;
 		}
 		this.weight = weight;
@@ -45,6 +49,7 @@ public abstract class InputObject extends GraphObject implements HasIntensity {
 
 	/**
 	 * Returns the weight
+	 *
 	 * @return the weight
 	 */
 	public Double getWeight() {
@@ -55,15 +60,15 @@ public abstract class InputObject extends GraphObject implements HasIntensity {
 	 * {@inheritDoc }
 	 */
 	@Override
-	public IntensityProfile getIntensityProfile(){
+	public IntensityProfile getIntensityProfile() {
 		return intensities.clone();
 	}
-	
+
 	/**
 	 * {@inheritDoc }
 	 */
 	@Override
-	public void setIntensityProfile(IntensityProfile profile){
+	public void setIntensityProfile(IntensityProfile profile) {
 		intensities = profile;
 	}
 
@@ -93,5 +98,17 @@ public abstract class InputObject extends GraphObject implements HasIntensity {
 	 */
 	public final Float[] getRawIntensitiesAsFloat() {
 		return intensities.getRawIntensitiesAsFloat();
+	}
+
+	public String getAnnotation() {
+		return annotation_id;
+	}
+
+	public void setAnnotation(String annotation_id) {
+		this.annotation_id = annotation_id;
+	}
+
+	public boolean hasAnnotation() {
+		return annotation_id != null;
 	}
 }
