0c97277183de2b53013a7b8a5ac525138957a04f
Manuel Landesfeind <manuel@gobics.de>
2013-05-17 15:19:41 +0200

    Add automatic annotation

--- src/de/gobics/marvis/graph/gui/tasks/ImportTranscriptomics.java
+++ src/de/gobics/marvis/graph/gui/tasks/ImportTranscriptomics.java
@@ -1,16 +1,13 @@
 package de.gobics.marvis.graph.gui.tasks;
 
+import de.gobics.marvis.graph.Gene;
 import de.gobics.marvis.graph.InputObject;
 import de.gobics.marvis.graph.MetabolicNetwork;
+import de.gobics.marvis.graph.Transcript;
 import de.gobics.marvis.utils.io.TabularDataReader;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 public class ImportTranscriptomics extends ImportAbstract {
-
-	private static final Logger logger = Logger.getLogger(ImportTranscriptomics.class.
-			getName());
-	private MetabolicNetwork network;
 
 	public ImportTranscriptomics(MetabolicNetwork graph, TabularDataReader reader) {
 		super(graph, reader);
@@ -20,6 +17,22 @@ public class ImportTranscriptomics extends ImportAbstract {
 
 	@Override
 	protected InputObject createObject(int row, String id, Object[] data) throws IOException {
-		return getNetwork().createTranscript(id);
+		Transcript t = getNetwork().createTranscript(id);
+
+		Integer idx = getAnnotationColumnIndex();
+		if (idx != null && idx >= 0) {
+			String gid = assertString(row, idx, data);
+			System.out.print("Annotation string is: " + gid);
+			if (gid != null) {
+				Gene g = getNetwork().getGene(gid);
+				System.out.print(" => " + gid.toString());
+				if (g != null) {
+					getNetwork().isFrom(t, g);
+				}
+			}
+			System.out.println();
+		}
+
+		return t;
 	}
 }
