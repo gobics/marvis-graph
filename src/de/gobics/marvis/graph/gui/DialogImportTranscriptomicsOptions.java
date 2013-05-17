73aa5b4f049a701049c7efa43d03fd3921c7859f
Manuel Landesfeind <manuel@gobics.de>
2013-05-17 15:19:13 +0200

    Remove debug output

--- src/de/gobics/marvis/graph/gui/DialogImportTranscriptomicsOptions.java
+++ src/de/gobics/marvis/graph/gui/DialogImportTranscriptomicsOptions.java
@@ -4,7 +4,6 @@ import de.gobics.marvis.graph.MetabolicNetwork;
 import de.gobics.marvis.graph.gui.tasks.ImportAbstract;
 import de.gobics.marvis.graph.gui.tasks.ImportTranscriptomics;
 import de.gobics.marvis.utils.io.TabularDataReader;
-import javax.swing.JPanel;
 
 public class DialogImportTranscriptomicsOptions extends DialogImport {
 
