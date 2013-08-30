7132c91962f19192ca3ab2a6df7d287277f11d69
Manuel Landesfeind <manuel@gobics.de>
2013-08-30 13:07:37 +0200

    Finish documentation

--- src/de/gobics/marvis/graph/gui/actions/ActionPermutationTest.java
+++ src/de/gobics/marvis/graph/gui/actions/ActionPermutationTest.java
@@ -7,6 +7,8 @@ package de.gobics.marvis.graph.gui.actions;
 import de.gobics.marvis.graph.gui.MarvisGraphMainWindow;
 import de.gobics.marvis.graph.gui.TreeModelNetworks;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.KeyStroke;
 
 /**
  *
@@ -16,7 +18,10 @@ public class ActionPermutationTest extends AbstractMarvisAction {
 	
 	public ActionPermutationTest(final MarvisGraphMainWindow window, final TreeModelNetworks tree_graphs) {
 		super(window, "Permutation Test");
+		
 		putValue(SHORT_DESCRIPTION, "Performs a permutation test on the found sub-networks");
+		putValue(MNEMONIC_KEY, KeyEvent.VK_P);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
 		
 		new EnableActionOnSubnetworks(this, tree_graphs);
 	}
